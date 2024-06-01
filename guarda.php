062d5262cbd9e25f844edf15505aa84aa4fb9fc6
Olger <oquispev@ulasalle.edu.pe>
2024-06-01 13:54:48 -0500

    admin editar y eliminacion

--- guarda.php
+++ guarda.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * CRUD modal en PHP y MySQL
- * 
- * Este archivo guarda los datos el registro y la imagen
- * @author MRoblesDev
- * @version 1.0
- * https://github.com/mroblesdev
- * 
- */
 
 session_start();
 
